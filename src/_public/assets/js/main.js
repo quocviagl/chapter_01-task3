555fc9aab00960a43bfb135b34e7d3bfa0176b96
Quoc-Vi <quocvi.agl@gmail.com>
2022-12-15 17:23:14 +0700

    Update Index1 + Done animation

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -1,1 +1,21 @@
-console.log("main.js");
+$(".c-point__iconmore").click(function(){
+            $(this).parent().parent().parent().find(".c-item__content").css("display", "none");
+            $(this).next().css("display","block");      
+           
+    });
+// ANIMATION
+$(window).scroll(function(){
+        if ($(this).scrollTop() === 0){
+
+                $(".c-leaf__topleaf").removeClass("c-leaf__animationy");
+                $(".c-leaf__sideleaf").removeClass("c-leaf__animationx");
+                $(".c-bubblesection").removeClass("is-hidden");
+
+
+        } else {
+                $(".c-leaf__topleaf").addClass("c-leaf__animationy");
+                $(".c-leaf__sideleaf").addClass("c-leaf__animationx");
+                $(".c-bubblesection").addClass("is-hidden");
+        }
+
+      });
